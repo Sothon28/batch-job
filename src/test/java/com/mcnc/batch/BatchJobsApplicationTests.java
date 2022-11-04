a7e4a0d540d3b0828f6c63928926c1c94707b579
Sothon-khmer <sothonthoeurn0@gmail.com>
2022-11-04 13:40:26 +0700

    first commit

---
+++ src/test/java/com/mcnc/batch/BatchJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcnc.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchJobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
